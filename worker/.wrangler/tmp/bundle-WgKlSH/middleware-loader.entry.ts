943d6ea4f1aa4f81e4dbddf4c1f65397bb0ccca9
Shane Oh <this.shaneoh@gmail.com>
2026-03-09 22:10:29 +0900

    add: codes

--- worker/.wrangler/tmp/bundle-WgKlSH/middleware-loader.entry.ts
+++ worker/.wrangler/tmp/bundle-WgKlSH/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/shane/workspace/ohshane.github.io/worker/.wrangler/tmp/bundle-6uVPHi/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/shane/workspace/ohshane.github.io/worker/.wrangler/tmp/bundle-WgKlSH/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/shane/.npm/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/shane/workspace/ohshane.github.io/worker/.wrangler/tmp/bundle-6uVPHi/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/shane/workspace/ohshane.github.io/worker/.wrangler/tmp/bundle-WgKlSH/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/shane/workspace/ohshane.github.io/worker/.wrangler/tmp/bundle-6uVPHi/middleware-insertion-facade.js";
+export * from "/Users/shane/workspace/ohshane.github.io/worker/.wrangler/tmp/bundle-WgKlSH/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
--- worker/.wrangler/tmp/dev-Jho44R/index.js
+++ worker/.wrangler/tmp/dev-Jho44R/index.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-6uVPHi/checked-fetch.js
+// .wrangler/tmp/bundle-WgKlSH/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -136,7 +136,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-6uVPHi/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-WgKlSH/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -168,7 +168,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-6uVPHi/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-WgKlSH/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
